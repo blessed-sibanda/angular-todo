4b198bdc1f89bd05d3217580461ec09ffd9b9b06
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-16 18:26:15 +0200

    create app data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './todo-item';
+import { TodoList } from './todo-list';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo';
+  private list = new TodoList('Blessed', [
+    new TodoItem('Go for a run', true),
+    new TodoItem('Watch a movies'),
+    new TodoItem('Read a book'),
+  ]);
+
+  get username(): string {
+    return this.list.user;
+  }
+
+  get itemCount(): number {
+    return this.list.items.filter((item) => !item.complete).length;
+  }
 }
